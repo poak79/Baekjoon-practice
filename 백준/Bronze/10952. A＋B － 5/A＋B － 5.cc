7875f2066e4017f153af398101398a661e180c5c
poak79 <parkyunseo5@gmail.com>
2025-11-12 22:52:31 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a=0, b=0;
+
+	while (true) {
+		cin >> a >> b;
+		if (a == 0 && b == 0) {
+			break;
+		}
+		else cout << a + b << endl;
+	}
+	
+}
