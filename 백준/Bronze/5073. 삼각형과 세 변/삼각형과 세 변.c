9ef35b54fa675d5f928dea9208b41bf734069baa
poak79 <parkyunseo5@gmail.com>
2025-12-24 15:29:45 +0900

    [Bronze III] Title: 삼각형과 세 변, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/5073. 삼각형과 세 변/삼각형과 세 변.c
@@ -0,0 +1,22 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a, b, c;
+
+	while (1) {
+		scanf("%d %d %d", &a, &b, &c);
+
+		if (a == 0 && b == 0 && c == 0) break;
+
+		int max = a;
+		
+		if (b > max) max = b;
+		if (c > max) max = c;
+		
+		if (max >= a + b + c - max) printf("Invalid\n");
+		else if (a == b && b == c) printf("Equilateral\n");
+		else if (a == b || a == c || b == c) printf("Isosceles\n");
+		else printf("Scalene\n");
+	}
+}
