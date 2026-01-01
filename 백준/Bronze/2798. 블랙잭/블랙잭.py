7a94076d71b7bd6343ee26d59628f379380004e2
poak79 <parkyunseo5@gmail.com>
2026-01-01 20:03:09 +0900

    [Bronze II] Title: 블랙잭, Time: 72 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -0,0 +1,15 @@
+n, m = map(int, input().split())
+cards = list(map(int, input().split()))
+
+result = 0
+
+for i in range(n):
+    for j in range(i+1, n):
+        for k in range(j+1, n):
+            sum = cards[i] + cards[j] + cards[k]
+
+            if sum <= m:
+                if sum > result:
+                    result = sum
+
+print(result)
