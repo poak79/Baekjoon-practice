abd8e9fa91746dff0dc565c5f525a65983d6741b
poak79 <parkyunseo5@gmail.com>
2026-01-30 19:13:43 +0900

    [Silver IV] Title: 제로, Time: 68 ms, Memory: 33192 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+k = int(input())
+
+stack = []
+
+for _ in range(k):
+    a = int(input())
+    if a == 0:
+        stack.pop()
+    else:
+        stack.append(a)
+
+print(sum(stack))
